97ba8feb1f7473bb557da4ec870e8a40bdf9e742
capturepointer <capturepointer@foxmail.com>
2015-12-31 23:01:21 +0800

    add check function and type function

--- sms_driver.php
+++ sms_driver.php
@@ -1,6 +1,7 @@
 <?php
 namespace capturePointer;
 class Sms_Driver{
+    protected $_type = "";
     protected $_apiKey = '';
     protected $_sendUrl = '';
 
@@ -9,25 +10,52 @@ class Sms_Driver{
         
     }
     
-    /*
-     * check user input sms captcha
-     */
-    public function check($captcha){
-        return false;
+    public function setType($type = 'normal') {
+        $this->_type = $type;
+        return true;
     }
-    
     /*
      * send sms
      */
-    public function send($phone, $msg){
+    public function send($phone, $msg, $captcha=''){
         return false;
     }
     
     /*
-     * get balance
+     * send on success
      */
-    public function getBalance(){
-        return false;
+    protected function _sendOnSuccess($phone,$captcha){
+        if(empty($captcha)){
+            return false;
+        }
+        else{
+            $this->__sessionId = session_id();
+            if(empty($this->__sessionId)) {
+                session_start();
+            }
+            $key = "capturePointer_sms_" . $this->_type . $phone . "_captcha"; 
+            $_SESSION[$key] = $captcha;
+            return true;
+        }
     }
+    
+    /*
+     * check user input sms captcha
+     */
+    public function check($phone, $captcha){
+        $this->__sessionId = session_id();
+        if(empty($this->__sessionId)) {
+            session_start();
+        }
+        $key = "capturePointer_sms_" . $this->_type . $phone . "_captcha"; 
+        if($_SESSION[$key] == $captcha){
+            $_SESSION[$key] = NULL;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
     
 }
--- subdrivers/sms_luosimao_driver.php
+++ subdrivers/sms_luosimao_driver.php
@@ -5,14 +5,19 @@
 */
 namespace capturePointer;
 class Sms_Luosimao_Driver extends \capturePointer\Sms_Driver{
-    protected $_apiKey = '8de8b94b1a006d1dc80asdfsdffae0';
+    protected $_apiKey = '';
     protected $_sendUrl = "http://sms-api.luosimao.com/v1/send.json";
     
     public function __construct() {
         
     }
     
-    public function send($phone, $msg){
+    public function init($apiKey, $sendUrl){
+        $this->_apiKey = $apiKey;
+        $this->_sendUrl = $sendUrl;
+    }
+    
+    public function send($phone, $msg, $captcha=''){
         if(!preg_match('/1\d{10}/', $phone) || strlen($phone) != 11 || empty($msg) || strlen($msg) > 500){
             return false;
         }
@@ -32,9 +37,10 @@ class Sms_Luosimao_Driver extends \capturePointer\Sms_Driver{
 
         $res = curl_exec( $ch );
         curl_close( $ch );
-        //$res  = curl_error( $ch );
+        $res  = curl_error( $ch );
         $json = json_decode($res);
         if(isset($json->error) && 0 == $json->error){
+            $this->_sendOnSuccess($phone,$captcha);
             return true;
         }
         else{
